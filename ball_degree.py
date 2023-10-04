05045b8771f4533881931b7b5581d3edb93e3c3d
KristevT <timkristev.cloud@gmail.com>
2023-10-04 14:32:11 +0300

    Remade code to flake8

--- ball_degree.py
+++ ball_degree.py
@@ -1,5 +1,8 @@
+"""Ball movement module."""
+
+
 def degree(radius: float, acceleration: float, time: float, velocity: float = 0):
-    """Finds the displacement degree of the point of initial contact with the surface.
+    """Finds the displacement degree of the point.
 
     Args:
         radius: float - radius of the rolling ball,
@@ -10,7 +13,9 @@ def degree(radius: float, acceleration: float, time: float, velocity: float = 0)
     Returns:
         degree: float - displacement degree of the point."""
 
-    s = velocity * time + (acceleration * (time ** 2)) / 2
-    c = 2 * 3.14 * radius
-    new_degree = round(((s % c) / c) * 360, 2)
+    if radius == 0:
+        return 0
+    distance = velocity * time + (acceleration * (time ** 2)) / 2
+    circle_length = 2 * 3.14 * radius
+    new_degree = round(((distance % circle_length) / circle_length) * 360, 2)
     return new_degree
